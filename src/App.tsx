0f1c3759a40ed840510aa2d87d91d26ac6918e01
sampet93 <47121537+sampet93@users.noreply.github.com>
2022-10-01 13:08:02 +0300

    Add loading spinner for loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import FormikSearchForm from "./components/FormikSearchForm";
 import ResultsArea from "./components/ResultsArea";
+import { CircularProgress } from "@mui/material";
+import { useAppSelector } from "./hooks/useTypedSelector";
 
 const App: React.FC = () => {
+  const { loading, error } = useAppSelector((state) => state);
   return (
     <div>
       <FormikSearchForm />
       <ResultsArea />
+      {loading && <CircularProgress />}
     </div>
   );
 };
